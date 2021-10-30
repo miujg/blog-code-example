6c83d425e38845c7111b7433ab3879c557b5b680
miujg <1066647329@qq.com>
2021-10-30 17:28:06 +0800

    some change

--- algorithm-stu/src/index.js
+++ algorithm-stu/src/index.js
@@ -1,58 +1,43 @@
-// 异或
-// 结合律 交换律 （无进位计算）
+// 各种排序 时间复杂度 等。。。。
 
-// let a = 10 
-// let b = 11
-
-// 使用异或来交换数据 不需要开辟新空间
-// 交换的两个变量在内存中的地址不一样
-// a = a ^ b
-// b = a ^ b
-// a = a ^ b
-
-// 异或题目
-// 一个数组， 一个数出现奇数次  其他数出现偶数次
-// 在时间复杂度O(N) 空间复杂度 O(1)的要求下
-// 使用异或操作符写出程序 (秒呀)
-
-// let arr = [1, 1, 1, 0, 0, 2, 2, 2, 3]
-// let eor = 0
-// let result
-// arr.forEach(item => result = item ^ eor)
-
-
-// 一个数组， 两个数出现奇数次  其他数出现偶数次
-// 在时间复杂度O(N) 空间复杂度 O(1)的要求下
-// 使用异或操作符写出程序 (秒呀)
-
-let arr1 = [1,1,1, 3,3,3, 2,2,2,2, 4,4, 6,6]
-
-function printOddTimesNum2(arr) {
-  let eor = 0
-  arr.forEach(item => eor = item ^ eor)
-  // 找到 eor 从右边数起第一位为1的项
-  // ~取反， &与
-  // 结果为 2的n次方 这个n就是右边起第几位
-  let rightOne = eor & (~eor + 1)
-
-  let onlyOne = 0
-  arr.forEach(item => {
-    if((item & rightOne) != 0) {
-      debugger
-      onlyOne = onlyOne ^ item
-    }
-  })
-
-  let eor2 = onlyOne ^ eor
-
-  return { onlyOne,  eor2}
+// 公共方法 交换数组的两列
+function swap(arr, i, j) {
+  arr[i] = arr[i] ^ arr[j]
+  arr[j] = arr[i] ^ arr[j]
+  arr[i] = arr[i] ^ arr[j]
 }
 
-let { onlyOne, eor2 } = printOddTimesNum2(arr1)
-console.log(onlyOne, eor2)
+// 1) 选择
 
-// let a = 0b0011110110000 
-// console.log(a & (~a + 1))
+// 2） 冒泡
+
+// 3) 插入排序
+// 时间复杂度：O(n^2)
+
+let arr3 = [1,2,5,2,6,9,11,6,32]
+
+function insertionSort(arr) {
+  if(arr == null || arr.length < 2) return
+  for(let i = 1; i < arr.length; i++) {  // 0 ~ i范围有序
+    for(let j = i - 1; j > 0; j--) { // 数据交换
+      if(arr[j] > arr[j + 1] ) swap(arr, j, j+1)  
+    }
+  }
+}
+
+insertionSort(arr3)
+console.log(arr3)
+
+// 4) 二分法 查找某个数
+// 非前提： 有序数组（也可以非无序）
+// 时间复杂度： O(logN)
+
+// 4.1 找某一个数 二分查找 找到返回
+// 4.2 找>=某个数最左侧的位置 二分到底
+// 4.3 局部（定义）最小值
+
+
+// 5) 对数器
 
 
 
---
+++ algorithm-stu/src/异或&位运算.js
@@ -0,0 +1,58 @@
+// 异或  位运算  常见面试题
+// 结合律 交换律 （无进位计算）
+
+// let a = 10 
+// let b = 11
+
+// 使用异或来交换数据 不需要开辟新空间
+// 交换的两个变量在内存中的地址不一样
+// a = a ^ b
+// b = a ^ b
+// a = a ^ b
+
+// 异或题目
+// 一个数组， 一个数出现奇数次  其他数出现偶数次
+// 在时间复杂度O(N) 空间复杂度 O(1)的要求下
+// 使用异或操作符写出程序 (秒呀)
+
+// let arr = [1, 1, 1, 0, 0, 2, 2, 2, 3]
+// let eor = 0
+// let result
+// arr.forEach(item => result = item ^ eor)
+
+
+// 一个数组， 两个数出现奇数次  其他数出现偶数次
+// 在时间复杂度O(N) 空间复杂度 O(1)的要求下
+// 使用异或操作符写出程序 (秒呀)
+
+let arr1 = [1,1,1,1, 3,3,3, 2,2,2,2, 4,4, 6,6, 11]
+
+function printOddTimesNum2(arr) {
+  let eor = 0
+  arr.forEach(item => eor = item ^ eor)
+  // 找到 eor 从右边数起第一位为1的项
+  // ~取反， &与
+  // 1101101000  =》  1000 理解
+  let rightOne = eor & (~eor + 1)
+
+  let onlyOne = 0
+  arr.forEach(item => {
+    // 注意这里的判断 理解
+    if((item & rightOne) === rightOne) {
+      onlyOne = onlyOne ^ item
+    }
+  })
+
+  let eor2 = onlyOne ^ eor
+
+  return { onlyOne,  eor2}
+}
+
+let { onlyOne, eor2 } = printOddTimesNum2(arr1)
+console.log(onlyOne, eor2)
+
+// let a = 0b0011110110000 
+// console.log(a & (~a + 1))
+
+
+
