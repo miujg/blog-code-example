d6b19b405311d8b674ef8d3c525d4e6dc85bcd6c
miujg <1066647329@qq.com>
2021-11-08 18:51:24 +0800

    file config

--- vue-webpack/build/webpack.common.js
+++ vue-webpack/build/webpack.common.js
@@ -26,6 +26,24 @@ module.exports = {
         use: [
           'style-loader', 'css-loader'
         ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // 这里的单位是byte 字节
+              limit: 200 * 1024,
+              // 输出到相应的目录
+              outputPath: "img/",
+              // 图片可以单独加域名
+              // publicPath: '',
+              // 防止图片导入为一个对象
+              esModule: false,
+            }
+          }
+        ]
       }
     ]
   }
