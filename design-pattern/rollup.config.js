6cf5dc261c24a66e224c2a072e583276f0ef557f
miujg <1066647329@qq.com>
2022-02-11 16:01:16 +0800

    some change

---
+++ design-pattern/rollup.config.js
@@ -0,0 +1,24 @@
+import babel from 'rollup-plugin-babel'
+import serve from 'rollup-plugin-serve'
+import liverreload from 'rollup-plugin-livereload'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/main.js',
+    name: 'main',
+    sourcemap: true
+  },
+  plugins: [
+    liverreload(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    serve({
+      open: true,
+      openPage: '/public/index.html',
+      port: 3001,
+      contentBase: ''
+    })
+  ]
+}
